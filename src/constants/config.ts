d5da2ba46aaa89e24774f9fda25bc7c38752f1f0
underxcore <osmsad786@gmail.com>
2026-03-25 19:03:29 +0530

    chore: Update API_URL for production deployment

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,5 +1,6 @@
 export const CONFIG = {
-  API_URL: "http://10.7.17.176:3000/api",
+  // API_URL: "http://10.7.17.176:3000/api",
+  API_URL: "https://clipart-ai.onrender.com/api",
   MAX_IMAGE_SIZE: 5 * 1024 * 1024, // 5MB
   IMAGE_DIMENSION_LIMIT: 1024,
 };
